aad71e71c2b4b8d4f5c1bab8873c9e0396c7afac
Rigos07 <51322232+Rigos07@users.noreply.github.com>
2019-11-22 19:04:22 +0100

    Ouverture de fichier main

--- code/main.c
+++ code/main.c
@@ -2,9 +2,24 @@
 #include "functions.h"
 
 int main(int argc, char *argv[]) {
+	char *sourceName, *resultName;
+	FILE *sourceFile, *resultFile;
+
+
 	if(argc <= 1) {
-		printf("Arguments insuffisants.\nUsage : ./main [nom fichier source] [nom fichier résultat]");
-	} else if (){}
+		printf("Arguments insuffisants.\nUsage : ./main [nom fichier source] [nom fichier résultat]\n");
+	} else { 
+		if (argc == 2) {
+			printf("Nom du fichier résultat non trouvé : mis dans \"out.s\" par défaut\n");
+			resultName = "out.s";
+		} else {
+			resultName = argv[2];
+		}
+		
+		sourceName = argv[1];
+		sourceFile = ouvreOuQuitte(sourceName, "rb");
+		sourceFile = ouvreOuQuitte(resultName, "wb");
+	}
 
     return 1;
 }
