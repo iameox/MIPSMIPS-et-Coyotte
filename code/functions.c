a6a7bcca64240b7d23dfcfaf9c0841732a902292
Rigos07 <51322232+Rigos07@users.noreply.github.com>
2019-11-25 19:16:31 +0100

    Fix des ( ) + début de raisonnement pour la fonction de ses grands morts

--- code/functions.c
+++ code/functions.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "functions.h"
 #include "files.h"
+#include <string.h>
 
 void afficherN(char *c, int n) {
     int i;
@@ -16,7 +17,7 @@ void afficherN(char *c, int n) {
 int wordIndex(char *ins) {
     int i = 0;
 
-    while (ins[i] == ' ' || ins[i] == ',' || ins[i] == '$') {
+    while (ins[i] == ' ' || ins[i] == ',' || ins[i] == '$' || ins[i] == '(') {
         i++;
     }
 
@@ -27,7 +28,7 @@ int wordIndex(char *ins) {
 int wordLength(char *ins) {
     int i = 0, stop = 0;
 
-    while (ins[i] != ' ' && ins[i] != ',' && ins[i] != '#' && ins[i] != '\0' && !stop) {
+    while (ins[i] != ' ' && ins[i] != ',' && ins[i] != '#' && ins[i] != '(' && ins[i] != ')' && ins[i] != '\0' && !stop) {
         if (ins[i] == ':') {
             stop = 1;
         }
@@ -54,5 +55,37 @@ void MIPStoHex(char *ins, char hex[SIZE]) {
         }
 
         index += length;
+    }
+
+    tafonctionpetee(ins, indexes, lengths, hex);
+
+
+    printf("index : %d %d %d %d\n", indexes[0], indexes[1], indexes[2], indexes[3]);
+    printf("taille : %d %d %d %d\n", lengths[0], lengths[1], lengths[2], lengths[3]);
+
+    afficherN(ins + indexes[0], lengths[0]);
+    afficherN(ins + indexes[1], lengths[1]);
+    afficherN(ins + indexes[2], lengths[2]);
+    afficherN(ins + indexes[3], lengths[3]);
+}
+
+
+
+void tafonctionpetee(char *ins, int indexes[4], int lengths[4], char hex[SIZE]) {
+    char * instruction = ins + indexes[0];
+    char * rs = ins + indexes[1];
+    char * rt = ins + indexes[2];
+    char * rd = ins + indexes[3];
+
+
+    if( !strncmp(instruction,"ADD",lengths[0]) ){
+        hex[0] = '0';
+        hex[1] = hex(strToBinary(rs, length[1]) >> 3);
+        hex[2] = hex((strToBinary(rs, length[1]) & 7) | strToBinary(rt, length[1]) >> 4 );
+        hex[3] = '0-15';
+        hex[4] = '0-15';
+        hex[5] = '0-14';
+        hex[6] = '2';
+        hex[7] = '0';
     }
 }
--- code/functions.h
+++ code/functions.h
@@ -1,3 +1,4 @@
 #define SIZE 8
 
 void MIPStoHex(char *ins, char hex[SIZE]);
+void tafonctionpetee(char *ins, int indexes[4], int lengths[4], char hex[SIZE]);
