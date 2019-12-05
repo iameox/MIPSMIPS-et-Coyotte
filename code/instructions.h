a854ebf615c06eea431914bf8c183807abe60e17
IamEox <iameox@outlook.fr>
2019-12-05 17:24:19 +0100

    décalé instructions dans instructions.h

--- code/instructions.h
+++ code/instructions.h
@@ -1,3 +1,7 @@
+#define INS_NAMES {"ADD", "ADDI", "AND", "BEQ", "BGTZ", "BLEZ", "BNE", "DIV", "J", "JAL", "JR", "LUI", "LW", "MFHI", "MFLO", "MULT", "NOP", "OR", "ROTR", "SLL", "SLT", "SRL", "SUB", "SW", "XOR"}
+#define INS_POINTERS {&mips_add, &mips_addi, &mips_and, &mips_beq, &mips_bgtz, &mips_blez, &mips_bne, &mips_div, &mips_j, &mips_jal, &mips_jr, &mips_lui, &mips_lw, &mips_mfhi, &mips_mflo, &mips_mult, &mips_nop, &mips_or, &mips_rotr, &mips_sll, &mips_slt, &mips_srl, &mips_sub, &mips_sw, &mips_xor}
+#define INS_NUMBER 25
+
 int mips_add(int arg1, int arg2, int arg3);
 int mips_addi(int arg1, int arg2, int arg3);
 int mips_and(int arg1, int arg2, int arg3);
