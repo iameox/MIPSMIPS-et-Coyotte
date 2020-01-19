202f7e48b340fb208f4d1dfc7575190fba14c528
IamEox <iameox@outlook.fr>
2020-01-19 20:09:35 +0100

    ptn µk

--- code/memory.c
+++ code/memory.c
@@ -112,7 +112,7 @@ void writeMemory(memSlot **mem, uint32_t address, int32_t value) {
 
     for (i = 0; i < 4; i++) {
         writeByte(mem, address, value);
-        value <<= 8;
+        value >>= 8;
     }
 }
 
